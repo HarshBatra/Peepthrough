00b7ea811e91c704a445709b705e0f4a6936d521
HARSH BATRA <harshbat22@gmail.com>
2023-02-01 13:48:19 +0530

    fixed category section, made responsive, added category data and animations

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Home from "./Pages/Home"
+import Home from "./Pages/Home";
 import SingleBlog from "./Pages/SingleBlog";
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer"
-import Faq from "./Pages/Faq"
+import Footer from "./Components/Footer";
+import Faq from "./Pages/Faq";
 import ShopkeeperLogin from "./Pages/ShopkeeperLogin";
 import Advertise from "./Pages/Advertise";
 import Privacy from "./Pages/Privacy";
-import "./style.css"
-import "./input.css"
+import "./style.css";
+import "./input.css";
+import "./animation.css";
 import Contact from "./Pages/Contact";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -18,16 +19,16 @@ function App() {
       <div className="font-poppins">
         <Router>
           <Navbar />
-            <Routes>
-              <Route path='/' element = { <Home /> } />
-              <Route path='/blog' element={ <SingleBlog /> } />
-              <Route path='/blog/:id' element={ <SingleBlog /> } />
-              <Route path='/faq' element={ <Faq /> } />
-              <Route path='/privacy-policy' element = { <Privacy /> } />
-              <Route path='/contact-us' element = { <Contact /> } />
-              <Route path='/shopkeeper-login' element = { <ShopkeeperLogin /> } />
-              <Route path='/advertise' element = { <Advertise /> } />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog" element={<SingleBlog />} />
+            <Route path="/blog/:id" element={<SingleBlog />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/privacy-policy" element={<Privacy />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/shopkeeper-login" element={<ShopkeeperLogin />} />
+            <Route path="/advertise" element={<Advertise />} />
+          </Routes>
           <Footer />
         </Router>
       </div>
--- src/constants/CategoriesData.js
+++ src/constants/CategoriesData.js
@@ -1,87 +1,92 @@
 module.exports = [
-    {
-      key:1,
-      category:'KidsWear', 
-      icon:"./Assets/Images/Icons/Kidswear.png"
-    },
-    {
-      key:2,
-      category:'Cinema', 
-      icon:"./Assets/Images/Icons/cinema.png"
-    },
-    {
-      key:3,
-      category:'Clothing & Footwear', 
-      icon:"./Assets/Images/Icons/Clothes.png"
-    },
-    {
-      key:4,
-      category:'Electronics', 
-      icon:"./Assets/Images/Icons/Electronics.png"
-    },
-    {
-      key:5,
-      category:'KidsWear', 
-      icon:"./Assets/Images/Icons/Kidswear.png"
-    },
-    {
-      key:6,
-      category:'Cinema', 
-      icon:"./Assets/Images/Icons/cinema.png"
-    },
-    {
-      key:7,
-      category:'Clothing & Footwear', 
-      icon:"./Assets/Images/Icons/Clothes.png"
-    },
-    {
-      key:8,
-      category:'Electronics', 
-      icon:"./Assets/Images/Icons/Electronics.png"
-    },
-    {
-      key:9,
-      category:'KidsWear', 
-      icon:"./Assets/Images/Icons/Kidswear.png"
-    },
-    {
-      key:10,
-      category:'Cinema', 
-      icon:"./Assets/Images/Icons/cinema.png"
-    },
-    {
-      key:11,
-      category:'Clothing & Footwear', 
-      icon:"./Assets/Images/Icons/Clothes.png"
-    },
-    {
-      key:12,
-      category:'Electronics', 
-      icon:"./Assets/Images/Icons/Electronics.png"
-    },
-    {
-      key:13,
-      category:'Electronics', 
-      icon:"./Assets/Images/Icons/Electronics.png"
-    },
-    {
-      key:14,
-      category:'KidsWear', 
-      icon:"./Assets/Images/Icons/Kidswear.png"
-    },
-    {
-      key:15,
-      category:'Cinema', 
-      icon:"./Assets/Images/Icons/cinema.png"
-    },
-    {
-      key:16,
-      category:'Clothing & Footwear', 
-      icon:"./Assets/Images/Icons/Clothes.png"
-    },
-    {
-      key:17,
-      category:'Electronics', 
-      icon:"./Assets/Images/Icons/Electronics.png"
-    },
-  ];
+  {
+    key: 1,
+    category: "Clothing & Footwear",
+    icon: "./Assets/Images/Icons/Clothes.png",
+  },
+  {
+    key: 2,
+    category: "KidsWear",
+    icon: "./Assets/Images/Icons/Kidswear.png",
+  },
+  {
+    key: 3,
+    category: "Jewelery",
+    icon: "./Assets/Images/Icons/Jewelry.png",
+  },
+  {
+    key: 4,
+    category: "Food & Drinks",
+    icon: "./Assets/Images/Icons/Food.png",
+  },
+  {
+    key: 5,
+    category: "Beauty & Spa",
+    icon: "./Assets/Images/Icons/Spa.png",
+  },
+  {
+    key: 6,
+    category: "Electronics",
+    icon: "./Assets/Images/Icons/Electronics.png",
+  },
+  {
+    key: 7,
+    category: "Hardware",
+    icon: "./Assets/Images/Icons/Hardware.png",
+  },
+  {
+    key: 8,
+    category: "Real Estate",
+    icon: "./Assets/Images/Icons/RealEstate.png",
+  },
+  {
+    key: 9,
+    category: "Opticals",
+    icon: "./Assets/Images/Icons/Optics.png",
+  },
+  {
+    key: 10,
+    category: "Medical",
+    icon: "./Assets/Images/Icons/Medical.png",
+  },
+  {
+    key: 11,
+    category: "Gifts & Fashion Accessories",
+    icon: "./Assets/Images/Icons/gift.png",
+  },
+  {
+    key: 12,
+    category: "Toys & Stationery",
+    icon: "./Assets/Images/Icons/Toy.png",
+  },
+  {
+    key: 13,
+    category: "Banks",
+    icon: "./Assets/Images/Icons/Bank.png",
+  },
+  {
+    key: 14,
+    category: "Furnishings",
+    icon: "./Assets/Images/Icons/Furniture.png",
+  },
+  {
+    key: 15,
+    category: "Sports",
+    icon: "./Assets/Images/Icons/sports.png",
+  },
+  {
+    key: 16,
+    category: "Cinema",
+    icon: "./Assets/Images/Icons/cinema.png",
+  },
+  {
+    key: 17,
+    category: "Hotels",
+    icon: "./Assets/Images/Icons/Hotels.png",
+  },
+  {
+    key: 18,
+    category: "Miscellaneous",
+    icon: "./Assets/Images/Icons/misc.png",
+  },
+];
