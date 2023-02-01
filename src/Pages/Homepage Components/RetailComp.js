0ce7fa00064cd6854b48eac5c510a04eca4fed79
HARSH BATRA <harshbat22@gmail.com>
2023-02-01 16:52:22 +0530

    fixed ui of retail and blog section, made responsive

--- src/Pages/Homepage Components/RetailComp.js
+++ src/Pages/Homepage Components/RetailComp.js
@@ -1,28 +1,68 @@
-import React from 'react';
+import React from "react";
 
 function RetailComp() {
   return (
     <>
-    <div className='py-20 my-16' style={{'background': 'radial-gradient(rgba(19, 168, 158, 0.5) 0%, rgba(19, 168, 158, 0.25) 0.01%, rgba(19, 168, 158, 0) 100%)',}}>
-        <div className='container mx-auto'>
-          {/* <h1 className='text-center mb-16'><span className='text-4xl text-primary-blue font-black tracking-wider retailHeading'>Retail Shopping </span> <span className='text-4xl text-black tracking-wide'> made easy</span></h1> */}
-          <h1 className='text-center mb-16'><span className='text-4xl text-primary-blue font-bold tracking-wider'>Retail Shopping </span> <span className='text-4xl text-black tracking-wide'> made easy</span></h1>
-          <div className='flex justify-between'>
-            <div className='h-auto w-80 relative flex flex-col justify-center items-center'>
-              <img src="./Assets/Images/data-searching.png"  width="200" height="200" className='max-h-full max-w-full'/>
-              <h1 className='max-w-full h-auto break-words text-center mt-4 text-xl font-medium tracking-widest'><span className='text-primary-blue text-2xl font-extrabold tracking-widest'>Explore</span> our market database & plan in advance your shopping schedule.</h1>
+      <div
+        className="py-20 my-16"
+        style={{
+          background:
+            "radial-gradient(rgba(19, 168, 158, 0.5) 0%, rgba(19, 168, 158, 0.25) 0.01%, rgba(19, 168, 158, 0) 100%)",
+        }}
+      >
+        <div className="mx-10 items-center flex-col flex">
+          <h1 className="text-center md:mb-16 mb-10">
+            <span className="text-3xl md:text-4xl text-primary-blue font-bold tracking-wider">
+              Retail Shopping{" "}
+            </span>{" "}
+            <span className="text-2xl md:text-4xl text-black tracking-wide">
+              {" "}
+              made easy
+            </span>
+          </h1>
+          <div className="flex md:justify-around w-full md:flex-row flex-col">
+            <div className="h-auto w-80 relative flex flex-col justify-center items-center">
+              <img
+                src="./Assets/Images/data-searching.png"
+                className="w-28 h-28 md:w-44 md:h-44"
+                alt=""
+              />
+              <h1 className="max-w-full mx-10 h-auto break-words text-center mt-4 md:text-lg text-sm font-medium tracking-widest mb-6">
+                <span className="text-primary-blue md:text-xl text-base font-extrabold tracking-widest">
+                  Explore
+                </span>{" "}
+                our market database & plan in advance your shopping schedule.
+              </h1>
             </div>
-            <div className='h-auto w-80 relative flex flex-col justify-center items-center'>
-              <img src="./Assets/Images/location.png"  width="200" height="200" className='max-h-full max-w-full'/>
-              <h1 className='max-w-full h-auto break-words text-center mt-4 text-xl font-medium tracking-widest'><span className='text-primary-blue text-2xl font-extrabold tracking-widest'>Search</span> the product you need, before you begin your shopping streak.</h1>
+            <div className="h-auto w-80 relative flex flex-col justify-center items-center">
+              <img
+                src="./Assets/Images/location.png"
+                className="w-28 h-28 md:w-44 md:h-44"
+                alt=""
+              />
+              <h1 className="max-w-full mx-10 h-auto break-words text-center mt-4 md:text-lg text-sm font-medium tracking-widest mb-6">
+                <span className="text-primary-blue md:text-xl text-base font-extrabold tracking-widest">
+                  Search
+                </span>{" "}
+                the product you need, before you begin your shopping streak.
+              </h1>
             </div>
-            <div className='h-auto w-80 relative flex flex-col justify-center items-center'>
-              <img src="./Assets/Images/sale.png"  width="200" height="200" className='max-h-full max-w-full'/>
-              <h1 className='max-w-full h-auto break-words text-center mt-4 text-xl font-medium leading-7 tracking-widest'><span className='text-primary-blue text-2xl font-extrabold leading-9	tracking-widest'>Learn</span> about sales & discounts being offered before you step out.</h1>
+            <div className="h-auto w-80 relative flex flex-col justify-center items-center">
+              <img
+                src="./Assets/Images/sale.png"
+                className="w-28 h-28 md:w-44 md:h-44"
+                alt=""
+              />
+              <h1 className="max-w-full mx-10 h-auto break-words text-center mt-4 md:text-lg text-sm font-medium tracking-widest">
+                <span className="text-primary-blue md:text-xl text-base font-extrabold tracking-widest">
+                  Learn
+                </span>{" "}
+                about sales & discounts being offered before you step out.
+              </h1>
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </>
   );
 }
